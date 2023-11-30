a771558254ec3fce15545f7431bbdcaee8436a72
gianlucanied <gianluca.nieddu96@gmail.com>
2023-11-30 16:54:03 +0100

    Add: setTimeout 30 seconds

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 
 // Utilizzo della funzione per generare 5 numeri casuali compresi tra 1 e 10
-var numeriGenerati = generaNumeriCasuali(1, 10, 5);
+var numeriGenerati = generaNumeriCasuali(1, 100, 5);
 console.log(numeriGenerati);
 
 // Creiamo la var per i numeri random generati
@@ -13,15 +13,16 @@ const randomNumber = document.getElementById("random-number");
 // Stampiamoli in pagina
 randomNumber.innerHTML = numeriGenerati;
 
-
+// Impostiamo il timer di 30 secondi per i numeri generati
+setTimeout(nascondiNumero, 1000);
 
 // Funzioni
 
 // Funzione per creare numeri casuali
-function generaNumeriCasuali(min, max, quantita) {
+function generaNumeriCasuali(min, max, length) {
     var numeriCasuali = [];
 
-    for (var i = 0; i < quantita; i++) {
+    for (var i = 0; i < length; i++) {
         var numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
         numeriCasuali.push(numeroCasuale);
     }
@@ -29,4 +30,19 @@ function generaNumeriCasuali(min, max, quantita) {
     return numeriCasuali;
 }
 
+// Funzione per nascondere numeri
+function nascondiNumero() {
 
+    let time = 30;
+    clock = setInterval(
+        function(){
+            time--;
+            console.log(time);
+            if (time === 0) {
+                clearInterval(clock);
+                randomNumber.classList.add("nascondi");
+            } 
+        },
+        1000
+    );
+}
